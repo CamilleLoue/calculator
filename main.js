8cf918786bd1014ca91f7f86b3b3de3b55bc2d5d
Camille Loue <camille.loue28@gmail.com>
2025-02-15 10:55:55 +0000

    added the major logic of the calculator

---
+++ main.js
@@ -0,0 +1,100 @@
+const display = document.getElementById("display");
+const numbers = document.querySelectorAll(".numbers>div");
+const operators = document.querySelectorAll(".operators>div");
+const clear = document.getElementById("clear");
+const equal = document.getElementById("equal");
+
+let firstNumberSelected;
+let secondNumberSelected;
+let operatorSelected;
+let equalsClicked = false;
+let operatorClicked = false;
+
+display.textContent = 0;
+
+// operations logic
+function operate(firstNum, secondNum, operator) {
+  const operations = {
+    sum: (firstNum, secondNum) =>
+      Math.round((firstNum + secondNum) * 1000) / 1000,
+    subtract: (firstNum, secondNum) =>
+      Math.round((firstNum - secondNum) * 1000) / 1000,
+    multiply: (firstNum, secondNum) =>
+      Math.round(firstNum * secondNum * 1000) / 1000,
+    divide: (firstNum, secondNum) =>
+      Math.round((firstNum / secondNum) * 1000) / 1000,
+  };
+
+  if (operations[operator]) {
+    if (typeof firstNum === "string" || typeof secondNum === "string") {
+      return "You messed up somewhere. Start again.";
+    } else if (operator === "divide" && secondNum === 0) {
+      return "Come on, you cannot divide by 0";
+    } else {
+      const func = operations[operator];
+      return func(firstNum, secondNum);
+    }
+  } else {
+    return "Invalid operation";
+  }
+}
+
+// user interractions with the calculator
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    console.log("display content", display.textContent);
+    if (display.textContent === "0") {
+      equalsClicked = false;
+      display.textContent = number.textContent;
+    } else if (equalsClicked) {
+      equalsClicked = false;
+      display.textContent = "";
+      display.textContent = number.textContent;
+    } else {
+      display.textContent += number.textContent;
+    }
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (operatorClicked) {
+      secondNumberSelected = Number(display.textContent);
+      firstNumberSelected = operate(
+        firstNumberSelected,
+        secondNumberSelected,
+        operatorSelected
+      );
+      console.log("first number selected", firstNumberSelected);
+      operatorSelected = operator.id;
+      operatorClicked = true;
+      console.log("operator selected", operatorSelected);
+      display.textContent = "";
+    } else {
+      firstNumberSelected = Number(display.textContent);
+      console.log("first number selected", firstNumberSelected);
+      operatorSelected = operator.id;
+      operatorClicked = true;
+      console.log("operator selected", operatorSelected);
+      display.textContent = "";
+    }
+  });
+});
+
+clear.addEventListener("click", () => {
+  display.textContent = 0;
+  operatorClicked = false;
+});
+
+equal.addEventListener("click", () => {
+  secondNumberSelected = Number(display.textContent);
+
+  const result = operate(
+    firstNumberSelected,
+    secondNumberSelected,
+    operatorSelected
+  );
+  display.textContent = result;
+  equalsClicked = true;
+  operatorClicked = false;
+});
